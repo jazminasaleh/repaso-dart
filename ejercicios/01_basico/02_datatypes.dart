b6b81bfa643c4fec8f3f9dfa32da22ee91f0a4c8
jazmin saleh peña <jazminasaleh@gmail.com>
2022-09-09 15:04:26 -0500

    tipos de dfatos

--- ejercicios/01_basico/02_datatypes.dart
+++ ejercicios/01_basico/02_datatypes.dart
@@ -5,13 +5,9 @@ void main(List<String> args) {
   print(a.toString() + ' ' + c.toString());
 
 //!String
-/*
-*Hola niño hermoso 
-*te amo 
-*/
 
   String? nom = 'jasmin';
-  print(nom);
+  print('$nom $a');
 
   String multilinea = '''
   Hola mundo
@@ -26,18 +22,30 @@ void main(List<String> args) {
   bool isnotactive = !isactive;
   print(isnotactive);
 
-//**Listas */
+//**Listas [] */
   List<String> villanos = ['lex', 'red skull', 'doom'];
   villanos.add('jasmin');
-  print(villanos[2]);
+  villanos.add('jasmin');
+  villanos.add('jasmin');
+  villanos.add('jasmin');
+  print(villanos);
 
+  //? Se convirtio una lista en un set
   var villanoset = villanos.toSet();
   villanoset.add('lex');
   print(villanoset);
 
-  // TODO: sets
+  //? sets {}
   Set<String> villanos2 = {'lex', 'red skull', 'doom'};
+    villanos2.add('jasmin');
+    villanos2.add('jasmin');
+    villanos2.add('jasmin');
+    villanos2.add('jasmin');
   print(villanos2);
+
+  //* La diferncia entre List y Set es que set no permite agregar datos duplicados
+  //* en cambio list si deja datos duplicados
+  //* y si queremos quitar los duplicados en un list .toset()
 
   //!Maps llave: valor
   Map<int, dynamic> ironmane = {
